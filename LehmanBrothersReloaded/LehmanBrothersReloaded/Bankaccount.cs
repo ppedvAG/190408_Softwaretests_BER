56ba56aae2710d2528bc4b8e35de23906feaba3f
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-08 15:05:36 +0200

    Logik ausimplementiert

--- LehmanBrothersReloaded/LehmanBrothersReloaded/Bankaccount.cs
+++ LehmanBrothersReloaded/LehmanBrothersReloaded/Bankaccount.cs
@@ -4,27 +4,33 @@ namespace LehmanBrothersReloaded
 {
     public class Bankaccount
     {
-        private decimal defaultBalance;
-
         public Bankaccount()
         {
         }
 
         public Bankaccount(decimal defaultBalance)
         {
-            this.defaultBalance = defaultBalance;
+            if (defaultBalance < 0)
+                throw new ArgumentException();
+            this.Balance = defaultBalance;
         }
 
         public decimal Balance { get; set; }
 
         public void Deposit(decimal v)
         {
-            throw new NotImplementedException();
+            if (v < 0)
+                throw new ArgumentException();
+            Balance += v;
         }
 
         public void Withdraw(decimal v)
         {
-            throw new NotImplementedException();
+            if (v < 0)
+                throw new ArgumentException();
+            if (Balance - v < 0)
+                throw new InvalidOperationException();
+            Balance -= v;
         }
     }
 }
