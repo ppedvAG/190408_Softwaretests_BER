9e17f8562195a5ac62ca14cfd659f8a8cc81ff19
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-08 16:09:35 +0200

    IsOpen - Übung

---
+++ LehmanBrothersReloaded/LehmanBrothersReloaded.Tests/OpeningHoursTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LehmanBrothersReloaded.Tests
+{
+    [TestFixture]
+    public class OpeningHoursTests
+    {
+        [Test]
+        [TestCase(2019, 4, 8, 10, 29, 00, false)]// Mo
+        [TestCase(2019, 4, 8, 10, 30, 00, true)] // Mo
+        [TestCase(2019, 4, 8, 12, 30, 00, true)] // Mo
+        [TestCase(2019, 4, 8, 19, 00, 00, true)] // Mo
+        [TestCase(2019, 4, 8, 19, 01, 00, false)] // Mo
+        [TestCase(2019, 4, 8, 22, 00, 00, false)] // Mo
+        [TestCase(2019, 4,9, 10, 29, 00, false)]// Di
+        [TestCase(2019, 4,9, 10, 30, 00, true)] // Di
+        [TestCase(2019, 4,9, 12, 30, 00, true)] // Di
+        [TestCase(2019, 4,9, 19, 00, 00, true)] // Di
+        [TestCase(2019, 4,9, 19, 01, 00, false)] //Di
+        [TestCase(2019, 4,9, 22, 00, 00, false)] //Di
+        [TestCase(2019, 4, 10, 10, 29, 00, false)]// Mi
+        [TestCase(2019, 4, 10, 10, 30, 00, true)] // Mi
+        [TestCase(2019, 4, 10, 12, 30, 00, true)] // Mi
+        [TestCase(2019, 4, 10, 19, 00, 00, true)] // Mi
+        [TestCase(2019, 4, 10, 19, 01, 00, false)] //Mi
+        [TestCase(2019, 4, 10, 22, 00, 00, false)] //Mi
+        [TestCase(2019, 4, 11, 10, 29, 00, false)]// Do
+        [TestCase(2019, 4, 11, 10, 30, 00, true)] // Do
+        [TestCase(2019, 4, 11, 12, 30, 00, true)] // Do
+        [TestCase(2019, 4, 11, 19, 00, 00, true)] // Do
+        [TestCase(2019, 4, 11, 19, 01, 00, false)] //Do
+        [TestCase(2019, 4, 11, 22, 00, 00, false)] //Do
+        [TestCase(2019, 4, 12, 10, 29, 00, false)]// Fr
+        [TestCase(2019, 4, 12, 10, 30, 00, true)] // Fr
+        [TestCase(2019, 4, 12, 12, 30, 00, true)] // Fr
+        [TestCase(2019, 4, 12, 19, 00, 00, true)] // Fr
+        [TestCase(2019, 4, 12, 19, 01, 00, false)] //Fr
+        [TestCase(2019, 4, 12, 22, 00, 00, false)] //Fr
+        [TestCase(2019, 4, 13, 10, 29, 00, false)]// Sa
+        [TestCase(2019, 4, 13, 10, 30, 00, true)] // Sa
+        [TestCase(2019, 4, 13, 12, 30, 00, true)] // Sa
+        [TestCase(2019, 4, 13, 14, 00, 00, true)] // Sa
+        [TestCase(2019, 4, 13, 14, 01, 00, false)] //Sa
+        [TestCase(2019, 4, 13, 22, 00, 00, false)] //Sa
+        [TestCase(2019, 4, 14, 10, 29, 00, false)]// So
+        [TestCase(2019, 4, 14, 10, 30, 00, false)] //So
+        [TestCase(2019, 4, 14, 12, 30, 00, false)] //So
+        [TestCase(2019, 4, 14, 14, 00, 00, false)] //So
+        [TestCase(2019, 4, 14, 14, 01, 00, false)] //So
+        [TestCase(2019, 4, 14, 22, 00, 00, false)] //So
+        public void OpeningHours_IsOpen(int year, int month, int day, int hour, int minute, int second, bool expectedResult)
+        {
+            var oh = new OpeningHours();
+            var dt = new DateTime(year, month, day, hour, minute, second);
+
+            Assert.AreEqual(expectedResult, oh.IsOpen(dt));
+        }
+    }
+}
---
+++ LehmanBrothersReloaded/LehmanBrothersReloaded/OpeningHours.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LehmanBrothersReloaded
+{
+    public class OpeningHours
+    {
+        public bool IsOpen(DateTime time)
+        {
+            switch (time.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                    // 10:30 - 19:00 offen
+                    if (time.Hour == 10) // Sonderfall: Punkt 10:30
+                        if (time.Minute >= 30)
+                            return true;
+                        else
+                            return false;
+                    if (time.Hour == 19) // Sonderfall: Punkt 19:00
+                        if (time.Minute == 00)
+                            return true;
+                        else
+                            return false;
+                    if (time.Hour >= 10 && time.Hour <= 18)
+                        return true;
+                    else
+                        return false;
+                case DayOfWeek.Saturday:
+                    // 10:30 - 14:00 offen
+                    if (time.Hour == 10)
+                        if (time.Minute >= 30)
+                            return true;
+                        else
+                            return false;
+                    if (time.Hour == 14) // Sonderfall: Punkt 14:00
+                        if (time.Minute == 00)
+                            return true;
+                        else
+                            return false;
+
+                    if (time.Hour >= 10 && time.Hour <= 13)
+                        return true;
+                    else
+                        return false;
+                default: // Sunday
+                    return false;
+            }
+        }
+    }
+}
