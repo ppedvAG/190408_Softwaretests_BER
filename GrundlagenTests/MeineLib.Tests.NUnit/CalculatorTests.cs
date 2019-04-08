8a758d3a76cb48cc0d42a68d6c5b31ca6aef50b8
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-08 11:24:39 +0200

    Erster NUnit Test

---
+++ GrundlagenTests/MeineLib.Tests.NUnit/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeineLib.Tests.NUnit
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        [Category("NUnit")]
+        public void Calculator_Sum_3_AND_5_Results_8()
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Sum(3, 5);
+            Assert.AreEqual(8, result);
+        }
+
+    }
+}
