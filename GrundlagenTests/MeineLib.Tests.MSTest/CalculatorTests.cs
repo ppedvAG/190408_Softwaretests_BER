8c83b907a32da901123a908ae5e7acf032e63329
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-08 10:39:59 +0200

    Der erste UnitTest

---
+++ GrundlagenTests/MeineLib.Tests.MSTest/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MeineLib.Tests.MSTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        // Klassenname_Methodenname_Szenario_erwartetesErgebnis
+        [TestMethod]
+        public void Calculator_Sum_3_AND_5_Results_8()
+        {
+            // Arrange
+            Calculator c = new Calculator();
+            // Act
+            int result = c.Sum(3, 5);
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+    }
+}
