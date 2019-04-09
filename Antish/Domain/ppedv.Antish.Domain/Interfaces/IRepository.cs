65a03180a3a613b884c38b159f9b66f80c972802
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-09 15:13:48 +0200

    IRepository erstellt

---
+++ Antish/Domain/ppedv.Antish.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.Antish.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        IEnumerable<T> GetAll<T>() where T : Entity;
+        T GetByID<T>(int ID) where T : Entity;
+        void Add<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        void Save();
+    }
+}
