d0f7e8a75aa7cc971f9b1bd7ffd9715a02e2f5bc
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-09 11:22:19 +0200

    Basisversion EF

--- Antish/Data/ppedv.Antish.Data.EF/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ppedv.Antish.Data.EF
-{
-    public class Class1
-    {
-    }
-}
---
+++ Antish/Data/ppedv.Antish.Data.EF/EFContext.cs
@@ -0,0 +1,18 @@
+﻿using ppedv.Antish.Domain;
+using System;
+using System.Data.Entity;
+
+namespace ppedv.Antish.Data.EF
+{
+    public class EFContext : DbContext
+    {
+        // Minimal-Version von EF
+        //public EFContext() : this(@"Server=.;Database=Antish_ProduktionsDB;Trusted_Connection=true") { }
+        public EFContext() : this(@"Server=(localDB)\MSSQLLocalDB;Database=Antish_ProduktionsDB;Trusted_Connection=true;AttachDbFileName=C:\temp\antish.mdf") { }
+        public EFContext(string connectionString) : base(connectionString){}
+
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Company> Company { get; set; }
+    }
+}
