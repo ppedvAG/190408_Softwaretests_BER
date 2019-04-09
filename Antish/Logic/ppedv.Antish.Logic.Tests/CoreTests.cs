3802d137295897b5d611fbf280ee0cd859770196
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-09 14:39:35 +0200

    Problemstellung: Core testen ohne echte Hardware zu verwenden

---
+++ Antish/Logic/ppedv.Antish.Logic.Tests/CoreTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.Antish.Logic.Tests
+{
+    [TestFixture]
+    public class CoreTests
+    {
+
+        [Test]
+        public void Core_RecruitPerson_can_recruit_5_Persons()
+        {
+            var core = new Core(null); // <---- Hier brauchen wir normalerweise Hardware
+
+            var result = core.RecruitPersons(5);
+
+            Assert.AreEqual(5, result.Count());
+        }
+
+    }
+}
--- Antish/Logic/ppedv.Antish.Logic/Core.cs
+++ Antish/Logic/ppedv.Antish.Logic/Core.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿using ppedv.Antish.Domain;
+using ppedv.Antish.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
 
 namespace ppedv.Antish.Logic
 {
-    public class Core
+    public class Core // Geschäftslogik
     {
+        public Core(IDevice device)
+        {
+            this.device = device;
+        }
+
+        private readonly IDevice device;
+
+        /* Irgendeine Logik, die meine Hardware intern verwendet
+         * Problem: Man muss den Core testen, ohne dass das echte Gerät (Device) angesteuert wird
+         * => Testgeräte verwenden
+         * Neues Problem: Es steht nicht überall ein Testgerät/Roboterarm etc zur Verfügung
+         * => Mocks
+         */
+        public List<Person> RecruitPersons(int amount)
+        {
+            List<Person> output = new List<Person>();
+            for (int i = 0; i < amount; i++)
+            {
+                output.Add(device.RecruitPerson()); // Echtgerät aufrufen
+            }
+            return output;
+        }
     }
 }
