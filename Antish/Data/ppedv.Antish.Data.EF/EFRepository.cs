2a8fbd13f17253600a9741c6cf9ddcaea7ce8496
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-09 15:25:22 +0200

    EFRepository erstellt

---
+++ Antish/Data/ppedv.Antish.Data.EF/EFRepository.cs
@@ -0,0 +1,43 @@
+﻿using ppedv.Antish.Domain;
+using ppedv.Antish.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.Antish.Data.EF
+{
+    public class EFRepository : IRepository
+    {
+        public EFRepository(EFContext context)
+        {
+            this.context = context;
+        }
+        private readonly EFContext context;
+
+        public void Add<T>(T item) where T : Entity
+        {
+            context.Set<T>().Add(item);
+        }
+
+        public void Delete<T>(T item) where T : Entity
+        {
+            context.Set<T>().Remove(item);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetByID<T>(int ID) where T : Entity
+        {
+            return context.Set<T>().Find(ID);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
