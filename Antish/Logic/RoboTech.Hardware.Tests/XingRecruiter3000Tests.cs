5ba72f56cd509a781cd4847facc66648169f8837
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-09 14:26:28 +0200

    Grundproblem: Testen von Hardware

---
+++ Antish/Logic/RoboTech.Hardware.Tests/XingRecruiter3000Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboTech.Hardware.Tests
+{
+    [TestFixture]
+    public class XingRecruiter3000Tests
+    {
+        [Test]
+        public void XingRecruiter3000_can_recruit_person()
+        {
+            var recruiter = new XingRecruiter3000();
+
+            var person = recruiter.RecruitPerson();
+
+            Assert.NotNull(person);
+
+            // Wenn der Test ausgeführt wird, piepst es !!!
+            // Problem: Bei einem UnitTest wollen wir die Maschine ja nicht in echt steuern
+            // Ziel: Simulieren, dass die Maschine etwas macht (piept)
+        }
+    }
+}
