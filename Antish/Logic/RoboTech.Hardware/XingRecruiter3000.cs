e304c1af32cb5dfa97a06c1da140388975a8f610
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-09 14:11:38 +0200

    Hardware erstellt

---
+++ Antish/Logic/RoboTech.Hardware/XingRecruiter3000.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using ppedv.Antish.Domain;
+using ppedv.Antish.Domain.Interfaces;
+using System;
+
+namespace RoboTech.Hardware
+{
+    public class XingRecruiter3000 : IDevice
+    {
+        private Fixture fix = new Fixture();
+
+        public Person RecruitPerson()
+        {
+            Console.Beep(10000,250);
+            Console.Beep(8000,250);
+            return fix.Create<Person>();
+        }
+    }
+}
